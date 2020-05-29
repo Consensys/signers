8f182f79212835d189ffac88d12cb4bdbeee27a3
Jason Frame <909467+jframe@users.noreply.github.com>
2020-05-29 15:42:08 +1000

    Add secp signing from EthSigner

--- signing/secp-signing-api/src/main/java/tech/pegasys/ethsigner/core/signing/TransactionSignerProvider.java
+++ signing/secp-signing-api/src/main/java/tech/pegasys/ethsigner/core/signing/TransactionSignerProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 ConsenSys AG.
+ * Copyright 2019 ConsenSys AG.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -10,14 +10,16 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+package tech.pegasys.ethsigner.core.signing;
 
-rootProject.name='signers'
-include 'bls-keystore'
-include 'keystorage:hashicorp'
-include 'acceptance-tests'
-include 'signing:secp-signing-api'
-include 'signing:secp-signer:common'
-include 'signing:secp-signer:azure'
-include 'signing:secp-signer:file-based'
-include 'signing:secp-signer:hashicorp'
-include 'signing:secp-signer:multikey'
+import java.util.Optional;
+import java.util.Set;
+
+public interface TransactionSignerProvider {
+
+  Optional<TransactionSigner> getSigner(String address);
+
+  Set<String> availableAddresses();
+
+  default void shutdown() {}
+}
