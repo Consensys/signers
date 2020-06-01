fb694ce14028b2d88635a6bcd739cdb1c944c2bd
Jason Frame <909467+jframe@users.noreply.github.com>
2020-06-01 14:23:17 +1000

    rename secpsigner package to secp256k1 package

---
+++ signing/secp256k1/multikey/src/main/java/tech/pegasys/signers/secp256k1/multikey/metadata/AzureSigningMetadataFile.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.signers.secp256k1.multikey.metadata;
+
+import tech.pegasys.signers.secp256k1.azure.AzureConfig;
+import tech.pegasys.signers.secp256k1.multikey.MultiSignerFactory;
+import tech.pegasys.signers.secp256k1.signerapi.TransactionSigner;
+
+public class AzureSigningMetadataFile extends SigningMetadataFile {
+
+  private final AzureConfig config;
+
+  public AzureSigningMetadataFile(final String filename, final AzureConfig config) {
+    super(filename);
+    this.config = config;
+  }
+
+  public AzureConfig getConfig() {
+    return config;
+  }
+
+  @Override
+  public TransactionSigner createSigner(final MultiSignerFactory factory) {
+    return factory.createSigner(this);
+  }
+}
