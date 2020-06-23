220e025f23cfac5e60e3b147f5780b54dd70d9ae
mkrielza <marelize.kriel@adhara.io>
2020-06-23 14:37:30 +0200

    Licenses and Acceptance Tests

---
+++ acceptance-tests/src/test/java/tech/pegasys/signers/secp256k1/tests/multikey/signing/MultiKeyCaviumTransactionSignerAcceptanceTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.signers.secp256k1.tests.multikey.signing;
+
+import static tech.pegasys.signers.secp256k1.MultiKeyTomlFileUtil.createCaviumTomlFileAt;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+public class MultiKeyCaviumTransactionSignerAcceptanceTest
+    extends MultiKeyTransactionSigningAcceptanceTestBase {
+
+  static final String FILENAME = "199dad4da603c24d55a9562ff680fbdea7ebd797";
+  static final String ADDRESS = "0x199DaD4da603C24D55A9562fF680FbDEa7Ebd797";
+
+  @Test
+  public void caviumLoadedFromMultiKeyCanSign(@TempDir Path tomlDirectory) {
+    createCaviumTomlFileAt(tomlDirectory.resolve("arbitrary_prefix" + FILENAME + ".toml"), ADDRESS);
+    setup(tomlDirectory);
+    verifySignature();
+  }
+}
---
+++ acceptance-tests/src/test/java/tech/pegasys/signers/secp256k1/tests/multikey/signing/MultiKeyHSMTransactionSignerAcceptanceTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.signers.secp256k1.tests.multikey.signing;
+
+import static tech.pegasys.signers.secp256k1.MultiKeyTomlFileUtil.createHSMTomlFileAt;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+public class MultiKeyHSMTransactionSignerAcceptanceTest
+    extends MultiKeyTransactionSigningAcceptanceTestBase {
+
+  static final String FILENAME = "6b56d4db090acb3edc4a334c4773056040e74dcd";
+  static final String ADDRESS = "0x6b56D4Db090aCB3eDc4a334C4773056040E74DcD";
+  static final String SLOT = "WALLET-001";
+
+  @Test
+  public void hsmLoadedFromMultiKeyCanSign(@TempDir Path tomlDirectory) {
+    createHSMTomlFileAt(
+        tomlDirectory.resolve("arbitrary_prefix" + FILENAME + ".toml"), ADDRESS, SLOT);
+    setup(tomlDirectory);
+    verifySignature();
+  }
+}
