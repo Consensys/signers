d73d52e8926e18b2b90b88f369e8eedcf17147ec
Usman Saleem <usman@usmans.info>
2020-07-28 23:34:19 +1000

    Fetch secret from Azure Key Vault
    Signed-off-by: Usman Saleem <usman@usmans.info>

---
+++ keystorage/azure/src/test/java/tech/pegasys/signers/azure/AzureKeyVaultTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.signers.azure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AzureKeyVaultTest {
+  private static final String CLIENT_ID = System.getenv("KS_AZURE_CLIENT_ID");
+  private static final String CLIENT_SECRET = System.getenv("KS_AZURE_CLIENT_SECRET");
+  private static final String TENANT_ID = System.getenv("KS_AZURE_TENANT_ID");
+  private static final String VAULT_NAME = System.getenv("KS_AZURE_VAULT_NAME");
+  private static final String SECRET_NAME = "TEST-KEY";
+  private static final String EXPECTED_KEY =
+      "3ee2224386c82ffea477e2adf28a2929f5c349165a4196158c7f3a2ecca40f35";
+
+  @BeforeAll
+  public static void setup() {
+    Assumptions.assumeTrue(CLIENT_ID != null, "Set KS_AZURE_CLIENT_ID environment variable");
+    Assumptions.assumeTrue(
+        CLIENT_SECRET != null, "Set KS_AZURE_CLIENT_SECRET environment variable");
+    Assumptions.assumeTrue(TENANT_ID != null, "Set KS_AZURE_TENANT_ID environment variable");
+    Assumptions.assumeTrue(VAULT_NAME != null, "Set KS_AZURE_VAULT_NAME environment variable");
+  }
+
+  @Test
+  void fetchExistingSecretKeyFromAzureVault() {
+    final AzureKeyVault azureKeyVault =
+        new AzureKeyVault(CLIENT_ID, CLIENT_SECRET, TENANT_ID, VAULT_NAME);
+    final Optional<String> hexKey = azureKeyVault.fetchSecret(SECRET_NAME);
+    assertThat(hexKey).isNotEmpty().get().isEqualTo(EXPECTED_KEY);
+  }
+
+  @Test
+  void connectingWithInvalidTenantIdThrowsException() {
+    final AzureKeyVault azureKeyVault =
+        new AzureKeyVault(CLIENT_ID, CLIENT_SECRET, "invalid", VAULT_NAME);
+    assertThatExceptionOfType(RuntimeException.class)
+        .isThrownBy(() -> azureKeyVault.fetchSecret(SECRET_NAME));
+  }
+
+  @Test
+  void connectingWithInvalidClientSecretThrowsException() {
+    final AzureKeyVault azureKeyVault =
+        new AzureKeyVault(CLIENT_ID, "invalid", TENANT_ID, VAULT_NAME);
+    assertThatExceptionOfType(RuntimeException.class)
+        .isThrownBy(() -> azureKeyVault.fetchSecret(SECRET_NAME));
+  }
+
+  @Test
+  void connectingWithInvalidClientIdThrowsException() {
+    final AzureKeyVault azureKeyVault =
+        new AzureKeyVault("invalid", CLIENT_SECRET, TENANT_ID, VAULT_NAME);
+    assertThatExceptionOfType(RuntimeException.class)
+        .isThrownBy(() -> azureKeyVault.fetchSecret(SECRET_NAME));
+  }
+
+  @Test
+  void nonExistingSecretReturnEmpty() {
+    final AzureKeyVault azureKeyVault =
+        new AzureKeyVault(CLIENT_ID, CLIENT_SECRET, TENANT_ID, VAULT_NAME);
+    assertThat(azureKeyVault.fetchSecret("X-" + SECRET_NAME)).isEmpty();
+  }
+}
