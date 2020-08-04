63dab76abb74a5b1abe6023f80ff1cc94cd53d6b
Trent Mohay <trent.mohay@consensys.net>
2020-08-04 11:56:40 +1000

    removed keyimporter

--- keystorage/azure/src/main/java/tech/pegasys/signers/azure/AzureKeyVault.java
+++ keystorage/azure/src/main/java/tech/pegasys/signers/azure/AzureKeyVault.java
@@ -75,6 +75,10 @@ public class AzureKeyVault {
         .buildClient();
   }
 
+  public KeyClient getKeyClient() {
+    return keyClient;
+  }
+
   public static String constructAzureKeyVaultUrl(final String keyVaultName) {
     return String.format(AZURE_URL_PATTERN, keyVaultName);
   }
