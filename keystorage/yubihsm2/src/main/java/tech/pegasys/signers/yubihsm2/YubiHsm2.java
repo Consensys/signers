ead6a5f0e22a237a2ff3b8f2179825256698a1f3
Usman Saleem <usman@usmans.info>
2020-09-17 15:43:04 +1000

    YubiHSM 2 connection logic
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- keystorage/yubihsm2/src/main/java/tech/pegasys/signers/yubihsm2/YubiHsm2.java
+++ keystorage/yubihsm2/src/main/java/tech/pegasys/signers/yubihsm2/YubiHsm2.java
@@ -12,9 +12,96 @@
  */
 package tech.pegasys.signers.yubihsm2;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.commons.io.IOUtils;
+import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.ProcessResult;
+
 /**
- * Access secrets (BLS Keys) from Yubi HSM 2. https://developers.yubico.com/YubiHSM2/ This class
- * uses yubihsm-shell which is a thin wrapper over libyubihsm. It is assumed that yubi hsm sdk is
- * installed on the system.
+ * Access secrets (BLS Keys) from Yubi HSM 2. https://developers.yubico.com/YubiHSM2/. It is assumed
+ * that yubi hsm sdk is installed on the system which contains yubihsm-shell. This class uses
+ * yubihsm-shell, which is a thin wrapper over libyubihsm, to talk to yubihsm.
  */
-public class YubiHsm2 {}
+public class YubiHsm2 {
+  private static final String YUBI_SHELL = "yubihsm-shell";
+  private final Optional<String> yubiHsmShellPath;
+  private final String connectorUrl;
+  private final short authKey;
+  private final String password;
+  private final Optional<String> caCert;
+  private final Optional<String> proxy;
+
+  /**
+   * Construct YubiHSM with parameters that will be passed to yubihsm-shell process
+   *
+   * @param connectorUrl YubiHSM connector URL. http://127.0.01:12345 or yhusb://serial=123456
+   * @param authKeyObjId The auth key object id
+   * @param password Password
+   * @param yubiHsmShellPath Optional path which contains yubihsm-shell binary. If empty current
+   *     directory will be used.
+   * @param caCert Optional CA Cert if connector is running in https mode
+   * @param proxy Optional proxy url if a proxy should be used to access connector url
+   */
+  public YubiHsm2(
+      final String connectorUrl,
+      final short authKeyObjId,
+      final String password,
+      final Optional<String> yubiHsmShellPath,
+      Optional<String> caCert,
+      Optional<String> proxy) {
+    this.yubiHsmShellPath = yubiHsmShellPath;
+    this.connectorUrl = connectorUrl;
+    this.authKey = authKeyObjId;
+    this.password = password;
+    this.caCert = caCert;
+    this.proxy = proxy;
+  }
+
+  public String fetchKey(final short opaqueObjId) throws YubiHsmException {
+    try {
+      return getOutputFromYubiHsmShell(buildCliArgs(opaqueObjId));
+    } catch (final IOException | TimeoutException | InterruptedException e) {
+      throw new YubiHsmException("Error in invoking yubihsm-shell process." + e.getMessage());
+    }
+  }
+
+  private String getOutputFromYubiHsmShell(final String[] args)
+      throws IOException, TimeoutException, InterruptedException {
+    final ProcessResult processResult =
+        new ProcessExecutor()
+            .command(args)
+            .readOutput(true)
+            .redirectInput(IOUtils.toInputStream(password + "\n", StandardCharsets.UTF_8.name()))
+            .destroyOnExit()
+            .execute();
+
+    final List<String> linesAsUTF8 = processResult.getOutput().getLinesAsUTF8();
+    if (processResult.getExitValue() == 0) {
+      return linesAsUTF8.get(linesAsUTF8.size() - 1);
+    }
+
+    throw new YubiHsmException(
+        "Unable to fetch data from YubiHSM: " + linesAsUTF8.get(linesAsUTF8.size() - 1));
+  }
+
+  private String[] buildCliArgs(final short opaqueObjectId) {
+    final ArrayList<String> args = new ArrayList<>();
+
+    args.add(Path.of(yubiHsmShellPath.orElse("."), YUBI_SHELL).toString());
+    args.add("--connector=" + connectorUrl);
+    args.add("--authkey=" + authKey);
+    args.add("--object-id=" + opaqueObjectId);
+    args.add("--action=get-opaque");
+    args.add("--outformat=hex");
+    caCert.ifPresent(s -> args.add("--cacert=" + s));
+    proxy.ifPresent(s -> args.add("--proxy=" + s));
+    return args.toArray(String[]::new);
+  }
+}
--- keystorage/yubihsm2/src/test/java/tech/pegasys/signers/yubihsm2/YubiHsm2Test.java
+++ keystorage/yubihsm2/src/test/java/tech/pegasys/signers/yubihsm2/YubiHsm2Test.java
@@ -12,13 +12,63 @@
  */
 package tech.pegasys.signers.yubihsm2;
 
+import java.util.Optional;
+
+import org.apache.commons.lang3.time.StopWatch;
+import org.assertj.core.api.Assertions;
 import org.assertj.core.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+// manual dev test - Make sure yubihsm2 is plugged in and connector daemon is running
+// uncomment system properties in setup method and modify accordingly
 class YubiHsm2Test {
+  private YubiHsm2 yubiHsm2;
+  private static final String BLS_KEY =
+      "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f";
+  private static final short OPAQUE_DATA_OBJECT_ID = (short) 4;
 
   @BeforeEach
   void setup() {
-    Assumptions.assumeThat(true).isTrue();
+    System.setProperty("yubihsmConnector", "http://localhost:12345");
+    System.setProperty("yubiHsmAuthId", "2");
+    System.setProperty("yubiHsmPassword", "password");
+    System.setProperty("yubiHsmPath", "/Users/usmansaleem/dev/yubihsm2-sdk/bin");
+
+    final String yubihsmConnector = System.getProperty("yubihsmConnector");
+    final String yubiHsmAuthId = System.getProperty("yubiHsmAuthId");
+    final String yubiHsmPassword = System.getProperty("yubiHsmPassword");
+    final String yubiShellPath = System.getProperty("yubiHsmPath");
+    final Optional<String> caCert = Optional.empty();
+    final Optional<String> proxy = Optional.empty();
+
+    Assumptions.assumeThat(yubihsmConnector).isNotEmpty();
+    Assumptions.assumeThat(yubiHsmAuthId).isNotEmpty();
+    Assumptions.assumeThat(yubiHsmPassword).isNotEmpty();
+    Assumptions.assumeThat(yubiShellPath).isNotEmpty();
+
+    yubiHsm2 =
+        new YubiHsm2(
+            yubihsmConnector,
+            Short.parseShort(yubiHsmAuthId),
+            yubiHsmPassword,
+            Optional.of(yubiShellPath),
+            caCert,
+            proxy);
+  }
+
+  @Test
+  void validDataIsReturnedFromYubiHSM() {
+
+    final StopWatch stopWatch = new StopWatch();
+    for (int i = 0; i < 10; i++) {
+      stopWatch.start();
+      final String blsKey = yubiHsm2.fetchKey(OPAQUE_DATA_OBJECT_ID);
+      stopWatch.stop();
+      System.out.println("Time Taken:" + stopWatch.toString());
+      stopWatch.reset();
+
+      Assertions.assertThat(blsKey).isEqualToIgnoringCase(BLS_KEY);
+    }
   }
 }
