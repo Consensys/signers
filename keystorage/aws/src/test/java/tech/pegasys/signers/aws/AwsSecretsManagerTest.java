1b3499c35b28f71cc1205b6a797a9e72e03289fe
georgep9 <g-patt@outlook.com>
2022-02-08 08:35:36 +1000

    unit tests for fetching secret value for seperate authentication modes
    Signed-off-by: georgep9 <g-patt@outlook.com>

--- keystorage/aws/src/test/java/tech/pegasys/signers/aws/AwsSecretsManagerTest.java
+++ keystorage/aws/src/test/java/tech/pegasys/signers/aws/AwsSecretsManagerTest.java
@@ -12,12 +12,15 @@
  */
 package tech.pegasys.signers.aws;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AwsSecretsManagerTest {
 
   private static final String SECRET_NAME =
@@ -26,10 +29,28 @@ class AwsSecretsManagerTest {
   private static final String EXPECTED_KEYSTORE =
       "{\"crypto\": {\"kdf\": {\"function\": \"scrypt\", \"params\": {\"dklen\": 32, \"n\": 262144, \"r\": 8, \"p\": 1, \"salt\": \"3d9b30b612f4f5e9423dc43c0490396798a179d35dd58d48dc1f5d6d42b07ab6\"}, \"message\": \"\"}, \"checksum\": {\"function\": \"sha256\", \"params\": {}, \"message\": \"c762b7453eab3332cda31d9dee1894cf541373617e591a8e7ab8f14f5830f723\"}, \"cipher\": {\"function\": \"aes-128-ctr\", \"params\": {\"iv\": \"095f79f6bb5daab60355ab6aa894b3c8\"}, \"message\": \"4ca342a769ec1c00d6a6d69e18cdf821f42849d4431da7df827b01ba162ed763\"}}, \"description\": \"\", \"pubkey\": \"8fb7c68f3291b8db46ef86a8b9544cad7052dd7cf817862063d1f151f3c443cd3907830b09a86fe0513f0e863beccf25\", \"path\": \"m/12381/3600/0/0/0\", \"uuid\": \"88fc9701-8670-4378-a3ba-00be25c1330c\", \"version\": 4}";
 
+  private AwsSecretsManager awsSecretsManagerDefault;
+  private AwsSecretsManager awsSecretsManagerExplicit;
+
+  @BeforeAll
+  void setupSecretManagers() {
+    final String awsRegion = "us-east-2";
+    final String awsAccessKeyId = System.getenv("AWS_ACCESS_KEY_ID");
+    final String awsSecretAccessKey = System.getenv("AWS_SECRET_ACCESS_KEY");
+    this.awsSecretsManagerDefault = AwsSecretsManager.createAwsSecretsManager(awsRegion);
+    this.awsSecretsManagerExplicit =
+        AwsSecretsManager.createAwsSecretsManager(awsAccessKeyId, awsSecretAccessKey, awsRegion);
+  }
+
   @Test
-  void fetchSecretValue() {
-    AwsSecretsManager awsSecretsManager = AwsSecretsManager.createAwsSecretsManager("us-east-2");
-    Optional<String> secret = awsSecretsManager.fetchSecretValue(SECRET_NAME, SECRET_KEY);
+  void fetchSecretValueWithDefaultManager() {
+    Optional<String> secret = awsSecretsManagerDefault.fetchSecretValue(SECRET_NAME, SECRET_KEY);
+    assertThat(secret).hasValue(EXPECTED_KEYSTORE);
+  }
+
+  @Test
+  void fetchSecretValueWithExplicitManager() {
+    Optional<String> secret = awsSecretsManagerExplicit.fetchSecretValue(SECRET_NAME, SECRET_KEY);
     assertThat(secret).hasValue(EXPECTED_KEYSTORE);
   }
 }
