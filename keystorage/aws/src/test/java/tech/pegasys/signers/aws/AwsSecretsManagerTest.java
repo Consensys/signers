a66a47332ce505322374c3e59b7279f8c1968cf2
georgep9 <g-patt@outlook.com>
2022-02-08 07:39:19 +1000

    removed cred
    Signed-off-by: georgep9 <g-patt@outlook.com>

--- keystorage/aws/src/test/java/tech/pegasys/signers/aws/AwsSecretsManagerTest.java
+++ keystorage/aws/src/test/java/tech/pegasys/signers/aws/AwsSecretsManagerTest.java
@@ -12,12 +12,11 @@
  */
 package tech.pegasys.signers.aws;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AwsSecretsManagerTest {
 
@@ -27,34 +26,10 @@ class AwsSecretsManagerTest {
   private static final String EXPECTED_KEYSTORE =
       "{\"crypto\": {\"kdf\": {\"function\": \"scrypt\", \"params\": {\"dklen\": 32, \"n\": 262144, \"r\": 8, \"p\": 1, \"salt\": \"3d9b30b612f4f5e9423dc43c0490396798a179d35dd58d48dc1f5d6d42b07ab6\"}, \"message\": \"\"}, \"checksum\": {\"function\": \"sha256\", \"params\": {}, \"message\": \"c762b7453eab3332cda31d9dee1894cf541373617e591a8e7ab8f14f5830f723\"}, \"cipher\": {\"function\": \"aes-128-ctr\", \"params\": {\"iv\": \"095f79f6bb5daab60355ab6aa894b3c8\"}, \"message\": \"4ca342a769ec1c00d6a6d69e18cdf821f42849d4431da7df827b01ba162ed763\"}}, \"description\": \"\", \"pubkey\": \"8fb7c68f3291b8db46ef86a8b9544cad7052dd7cf817862063d1f151f3c443cd3907830b09a86fe0513f0e863beccf25\", \"path\": \"m/12381/3600/0/0/0\", \"uuid\": \"88fc9701-8670-4378-a3ba-00be25c1330c\", \"version\": 4}";
 
-  private static AwsSecretsManager awsSecretsManager;
-
-  @BeforeAll
-  public static void setup() {
-    awsSecretsManager =
-        AwsSecretsManager.createAwsSecretsManager(
-            "AKIAXLE2XHFI6F6AZJGG", "SXY+YoMimF07iuWpAMrWpfZOcsz/0+3n4SUk0qqO", "us-east-2");
-  }
-
   @Test
   void fetchSecretValue() {
+    AwsSecretsManager awsSecretsManager = AwsSecretsManager.createAwsSecretsManager("us-east-2");
     Optional<String> secret = awsSecretsManager.fetchSecretValue(SECRET_NAME, SECRET_KEY);
     assertThat(secret).hasValue(EXPECTED_KEYSTORE);
   }
-
-  //  @Test
-  //  void nonExistentKeyStoreSecretReturnsNull() {
-  //    String keystore =
-  //        AwsSecretsManager.getKeyStoreValue(SECRETS_MANAGER_CLIENT, SECRET_NAME, "nonexistent");
-  //    assertThat(keystore).isNullOrEmpty();
-  //  }
-  //
-  //  @Test
-  //  void nonExistentSecretThrowsException() {
-  //    assertThatExceptionOfType(RuntimeException.class)
-  //        .isThrownBy(
-  //            () -> AwsSecretsManager.getKeyStoreValue(SECRETS_MANAGER_CLIENT, "empty",
-  // SECRET_KEY))
-  //        .withMessageContaining("Secrets Manager can't find the specified secret.");
-  //  }
 }
