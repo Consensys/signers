a4638e4175bce28fe37fa9c0500c150d7b5d3683
georgep9 <g-patt@outlook.com>
2022-03-22 16:35:56 +1000

    spotlessApply
    Signed-off-by: georgep9 <g-patt@outlook.com>

--- keystorage/aws/src/test/java/tech/pegasys/signers/aws/AwsSecretsManagerProviderTest.java
+++ keystorage/aws/src/test/java/tech/pegasys/signers/aws/AwsSecretsManagerProviderTest.java
@@ -12,14 +12,14 @@
  */
 package tech.pegasys.signers.aws;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AwsSecretsManagerProviderTest {
